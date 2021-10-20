4368055319d7ad1bb6d8758eb5215db014ecbec2
justjoshriley <joshriley316@gmail.com>
2021-10-19 18:35:00 -0700

    Paddle and Keyboard controls

--- index.js
+++ index.js
@@ -6,7 +6,38 @@ let y = canvas.height-30;
 let dx = 0;
 let dy = -2;
 
-var ballRadius = 10;
+let ballRadius = 10;
+
+
+// paddle
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width-paddleWidth) / 2;
+
+// paddle control
+let leftkeypressed = false;
+let rightkeypressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightkeypressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftkeypressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightkeypressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftkeypressed = false;
+    }
+}
 
 
 function drawBall() {
@@ -15,15 +46,37 @@ function drawBall() {
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
-
-    
 }
+
+// paddle
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
     x += dx;
     y += dy;
+
+    if(rightkeypressed) {
+        paddleX += 7;
+        if (paddleX + paddleWidth > canvas.width){
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+    else if(leftkeypressed) {
+        paddleX -= 7;
+        if (paddleX < 0){
+            paddleX = 0;
+        }
+    }
 
     // bouncing off the top or bottom
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
@@ -38,4 +91,6 @@ function draw() {
 
 }
 setInterval(draw, 10);
+
+
 
